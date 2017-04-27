f3922d47f50d93d92c1e8202c74f62bf9ee92d35
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-04-27 12:09:15 -0400

    Added database and database connexion

---
+++ data/connexionBD.php
@@ -0,0 +1,18 @@
+<?php
+	
+	//TODO : Mettre le bon fichier de database ici selon le projet
+	define('DATABASE_FILE', 'test.accdb');
+	
+	
+	$dbName =  dirname(__FILE__)."\\".DATABASE_FILE;
+		
+	if (!file_exists($dbName)) {
+	    die("Erreur, fichier de BD introuvable");
+	}
+	
+	$db = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};charset=UTF-8; DBQ=$dbName; Uid=; Pwd=;");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+?>
